db6088e7dc85403c61b44813509cbe18d3a0bbcb
Emirhan Kayar <emirhan.kayar80@gmail.com>
2025-04-22 03:13:29 +0200

    autocompletions

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,7 @@
 return {
     {
         "williamboman/mason.nvim",
+        lazy = false,
         config = function()
             require("mason").setup({
                 PATH = "prepend",
@@ -9,6 +10,7 @@ return {
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        lazy = false,
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = { "lua_ls", "ts_ls", "pyright" }
@@ -17,11 +19,19 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.ts_ls.setup({})
-            lspconfig.pyright.setup({})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities
+            })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
