ba8a9b2190ac146b92f561de695af573a459e699
Emirhan Kayar <emirhan.kayar80@gmail.com>
2025-04-25 00:59:23 +0200

    dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -3,11 +3,9 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
-
 	config = function()
 		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.startify")
-
+		local dashboard = require("alpha.themes.dashboard")
 		dashboard.section.header.val = {
 			[[                                                                       ]],
 			[[                                                                       ]],
@@ -27,5 +25,17 @@ return {
 		}
 
 		alpha.setup(dashboard.opts)
-	end
+		dashboard.section.header.opts.hl = "DashboardHeader"
+
+		dashboard.section.buttons.val = {
+            dashboard.button("e           ", "   New File", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("<leader> f f", "   Find File", ":Telescope find_files <CR>"),
+			dashboard.button("<leader> f t", "   Find Text", ":Telescope live_grep <CR>"),
+            dashboard.button("<leader> f h", "  Recent Files", ":Telescope oldfiles <CR>"),
+			dashboard.button("<leader> f u", "   Update Plugins", ":Lazy update <CR>"),
+			dashboard.button("<leader> e v", "   Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+			dashboard.button("           q", "   Quit Neovim", ":qa<CR>"),
+		}
+        --dashboard.section.footer.opts.hl = "DashboardFooter"
+	end,
 }
